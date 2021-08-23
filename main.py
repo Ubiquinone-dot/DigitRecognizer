b5e088bcbfe608737e82b88c5b03dbd65f6ee188
Jasper Butcher <JasperKBV@gmail.com>
2021-08-23 20:30:12 +0100

    NONE

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from src.Network1 import NET
 
 def main():
     nn = NET()
-    nn.load_data()
-    nn.train()
+    # nn.load_data()
+    nn.submit_latest_model()
+    #nn.train()
 
 
 if __name__ == '__main__':
--- src/Network1.py
+++ src/Network1.py
@@ -14,9 +14,19 @@ class NET:
 
         print(f'Network successfully built...\n\n')
 
-    @staticmethod
-    def submit_latest_model():
-        pass
+    def submit_latest_model(self):
+        self.reload_model()
+        x = self.load_data(SUB=True)
+        preds = np.argmax(self.model(x), axis=-1)
+
+        with open('data/submission.csv','w') as fhandle:
+            fhandle.write('ImageId,Label\n')
+            for i, pred in enumerate(preds):
+                fhandle.write(str(i+1)+','+str(pred)+'\n')
+
+        self.model.save(self.save_dir+'/latest_subbed_model')
+        print('Submission file updated and model saved')
+
 
     @staticmethod
     def make_model():
@@ -40,21 +50,20 @@ class NET:
 
             Flatten(),
             Dense(256),
-            Dropout(0.25),
+            Dropout(0.1),
             LeakyReLU(leak),
             Dense(28),
-            Dropout(0.25),
+            Dropout(0.1),
             LeakyReLU(leak),
             Dense(10),
-            Dropout(0.25),
             LeakyReLU(leak),
             Activation('softmax')
 
         ]]
 
         model.compile(
-            loss=tf.keras.losses.CategoricalCrossentropy(),
-            optimizer=tf.keras.optimizers.Nadam(learning_rate=0.001),
+            loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+            optimizer=tf.keras.optimizers.Adam(),
             metrics=['accuracy']
         )
 
@@ -75,8 +84,9 @@ class NET:
         print('Set up tensorboard log for: {}'.format(self.log_dir))
         # tensorboard --logdir logs/fit
 
-    def load_data(self):
+    def load_data(self, SUB=False):
         print('Loading data..')
+
         def load_file(SUB=False):
             if SUB: loc = 'data/test.csv'
             else: loc = 'data/train.csv'
@@ -98,37 +108,43 @@ class NET:
                         _labels.append(label)
 
                 X = np.array(X).astype('float32')
-                _labels = np.array(_labels).astype(int)
-
-                _labels = tf.keras.utils.to_categorical(_labels)
+                if not SUB:
+                    _labels = np.array(_labels).astype(int)
+                    _labels = tf.keras.utils.to_categorical(_labels)
+                else:
+                    _labels = np.array(_labels)
                 X = X / 255 - 0.5  # Normalize input
                 X = np.expand_dims(X, -1)  # Expand dims for convolutions
                 return X, _labels
 
-        x, labels = load_file()
+        x, labels = load_file(SUB)
         m = x.shape[0] * (x.shape[0] == labels.shape[0])
-        # shuffle
-        ind_list = list(range(m))
-        random.shuffle(ind_list)
-        x = x[ind_list][:,:]
-        labels = labels[ind_list]
+        if SUB:
+            print('Submission data successfully loaded')
+            return x
+        else:
+            # shuffle
+            ind_list = list(range(m))
+            random.shuffle(ind_list)
+            x = x[ind_list][:,:]
+            labels = labels[ind_list]
 
-        # Split the training file 90/10:
-        n = int(m * 0.9) # 37800, how many elements of the set go to training
-        train_x = x[:n]
-        test_x = x[n:]
-        train_labels = labels[:n]
-        test_labels = labels[n:]
-        assert(test_labels.shape[0] == test_x.shape[0])
-        assert(train_labels.shape[0] == train_x.shape[0])
+            # Split the training file 90/10:
+            n = int(m * 0.8) # 37800, how many elements of the set go to training
+            train_x = x[:n]
+            test_x = x[n:]
+            train_labels = labels[:n]
+            test_labels = labels[n:]
+            assert(test_labels.shape[0] == test_x.shape[0])
+            assert(train_labels.shape[0] == train_x.shape[0])
 
-        self.train_x = train_x
-        self.train_labels = train_labels
-        self.test_x = test_x
-        self.test_labels = test_labels
+            self.train_x = train_x
+            self.train_labels = train_labels
+            self.test_x = test_x
+            self.test_labels = test_labels
 
-        print('Data successfully loaded')
-        return (self.train_x, self.train_labels), (self.test_x, self.test_labels)
+            print('Training data successfully loaded')
+            return (self.train_x, self.train_labels), (self.test_x, self.test_labels)
 
     def train(self):
         print(self.test_labels.shape, '\n\n',self.test_labels)
